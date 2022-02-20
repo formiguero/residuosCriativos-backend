f88237863f7a0c89371b4dd10d859fb19cf45d85
Lucas Santos <lucas.lssg@gmail.com>
2022-02-20 00:24:29 -0300

    Adicionando página de resíduos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ResiduosComponent } from './residuos/residuos.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { CadastroPessoaFisicaComponent } from './cadastro/cadastro-pessoa-fisica/cadastro-pessoa-fisica.component';
@@ -13,7 +14,8 @@ const routes:Routes=[
   {path:'cadastro',component:CadastroComponent,children:[
     {path:'pessoa-fisica',component:CadastroPessoaFisicaComponent},
     {path:'pessoa-juridica',component:CadastroPessoaJuridicaComponent}
-  ]}
+  ]},
+  {path:'residuos',component:ResiduosComponent}
 ]
 
 @NgModule({
