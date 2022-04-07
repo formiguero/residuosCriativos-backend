58675ec16c63eb9531fe45acd1c025d1d50aa779
Lucas Gomes <lucas.lssg@gmail.com>
2022-04-07 11:04:21 -0300

    Corrigindo conflitos de versionamento|deploy|hosting|layot da home-page

--- src/app/cadastro/cadastro-pessoa-fisica/cadastro-pessoa-fisica.component.ts
+++ src/app/cadastro/cadastro-pessoa-fisica/cadastro-pessoa-fisica.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { UsersService } from 'src/app/shared/users.service';
 
 @Component({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
   apiKey: "AIzaSyCHAmMravBsX_xokVTLcbRL8lopMQxrcPc",
   authDomain: "residuoscriativos.firebaseapp.com",
