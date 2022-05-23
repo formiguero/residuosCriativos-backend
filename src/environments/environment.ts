c5917d2da224a6a14451793a37932c4e12f788fd
Lucas Gomes <lucas.lssg@gmail.com>
2022-05-23 14:17:31 -0300

    Corrigindo erro para realizar deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  firebaseConfig : {
-  apiKey: "AIzaSyCHAmMravBsX_xokVTLcbRL8lopMQxrcPc",
-  authDomain: "residuoscriativos.firebaseapp.com",
-  databaseURL: "https://residuoscriativos-default-rtdb.firebaseio.com",
-  projectId: "residuoscriativos",
-  storageBucket: "residuoscriativos.appspot.com",
-  messagingSenderId: "390129269654",
-  appId: "1:390129269654:web:244424e07160abc7dc7bb7",
-  measurementId: "G-6MSZ9SH15S"
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCHAmMravBsX_xokVTLcbRL8lopMQxrcPc",
+    authDomain: "residuoscriativos.firebaseapp.com",
+    databaseURL: "https://residuoscriativos-default-rtdb.firebaseio.com",
+    projectId: "residuoscriativos",
+    storageBucket: "residuoscriativos.appspot.com",
+    messagingSenderId: "390129269654",
+    appId: "1:390129269654:web:244424e07160abc7dc7bb7",
+    measurementId: "G-6MSZ9SH15S"
   }
 };
 
